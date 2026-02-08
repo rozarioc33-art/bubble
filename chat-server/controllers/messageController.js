fdba03a5e5ecdb8ad740aa874649d303df2472d9
Cyril Rozario <cyrilrozario7@gmail.com>
2026-02-08 18:01:50 +0530

    lastMessage preview

--- chat-server/controllers/messageController.js
+++ chat-server/controllers/messageController.js
@@ -1,4 +1,5 @@
 import Message from "../models/Message.js";
+import Chat from "../models/Chat.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -8,11 +9,18 @@ export const sendMessage = async (req, res) => {
             return res.status(400).json({message: "content and chatId required"});
         }
 
-        const message = await Message.create({
+        let message = await Message.create({
             sender: req.user._id,
             content,
             chat: chatId,
         });
+
+        await Chat.findByIdAndUpdate(chatId, {
+          lastMessage: message._id,
+        });
+
+        message = await message.populate("sender", "name email");
+        message = await message.populate("chat");
 
         res.status(201).json(message);
     } catch (error) {
