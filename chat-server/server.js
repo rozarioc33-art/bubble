58a7c91a1893745296ea14d4b2c2f5dffd6d1a5a
Cyril Rozario <cyrilrozario7@gmail.com>
2026-02-10 21:31:52 +0530

    socket setup

--- chat-server/server.js
+++ chat-server/server.js
@@ -7,10 +7,22 @@ import chatRoutes from "./routes/chatRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
+import { Server } from "socket.io";
+import http from "http";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(cors());
 app.use(express.json());
